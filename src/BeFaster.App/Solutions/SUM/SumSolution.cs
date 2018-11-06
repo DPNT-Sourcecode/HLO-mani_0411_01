1e67cdc9b89a804dd80e139b1f9d2d453fcdd2df
sbx_user1060 <sbx_user1060@ip-10-129-223-134.eu-west-2.compute.internal>
2018-11-06 00:39:14 +0000

    Tue Nov 06 00:39:14 UTC 2018

--- src/BeFaster.App/Solutions/SUM/SumSolution.cs
+++ src/BeFaster.App/Solutions/SUM/SumSolution.cs
@@ -6,7 +6,7 @@ namespace BeFaster.App.Solutions.SUM
     {
         public static int Sum(int x, int y)
         {
-            throw new SolutionNotImplementedException();
+            return x + y;
         }
     }
 }
